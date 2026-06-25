93d7bcf5b3e2a178bca006057d20ec25b0ded188
Obaloluwa Odelana <35340328+obaodelana@users.noreply.github.com>
2026-06-25 00:48:29 -0400

    use sign up link instead

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,2 +1,2 @@
-export const REGISTER_LINK = "https://enrol.adullamacademy.com";
+export const REGISTER_LINK = "https://aduaca.fillout.com/join-online";
 export const PARTNER_LINK = "https://aduaca.fillout.com/partner";
